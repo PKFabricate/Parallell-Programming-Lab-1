8cdaf8c9eb2f9e6b070b8cd4de4dffde2792e3dc
PKFabricate <151477741+PKFabricate@users.noreply.github.com>
2024-11-20 16:54:22 +0100

    Added my name to the Authors

--- source/authors_lab1.h
+++ source/authors_lab1.h
@@ -1,11 +1,11 @@
 #pragma once
 
-#define AUTHORS_TWO_A "Lukas Rothenberger"
-#define AUTHORS_TWO_B ""
-#define AUTHORS_TWO_C ""
-#define AUTHORS_TWO_D ""
-#define AUTHORS_TWO_E ""
-#define AUTHORS_TWO_F ""
+#define AUTHORS_TWO_A "Paul Klein"
+#define AUTHORS_TWO_B "Paul Klein"
+#define AUTHORS_TWO_C "Paul Klein"
+#define AUTHORS_TWO_D "Paul Klein"
+#define AUTHORS_TWO_E "Paul Klein"
+#define AUTHORS_TWO_F "Paul Klein"
 
 #define AUTHORS_THREE_A ""
 #define AUTHORS_THREE_B ""
