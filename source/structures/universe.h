843d588be594eeeff7784ecf79ed85c5d613b39e
PKFabricate <151477741+PKFabricate@users.noreply.github.com>
2024-11-20 16:44:11 +0100

    Aufgabe 2 Fertig REALLY

--- source/structures/universe.h
+++ source/structures/universe.h
@@ -1,5 +1,40 @@
 #pragma once
+#include <cstdint>
+#include <vector>
+#include "vector2d.h"
+#include "bounding_box.h"
+
 
 class Universe {
+	public:
+		std::uint32_t num_bodies = 0;
+		std::uint32_t current_simulation_epoch = 0;
+		std::vector<double> weights = std::vector<double>();
+		std::vector < Vector2d<double >> forces = std::vector<Vector2d<double >>();
+		std::vector < Vector2d<double >> velocities = std::vector<Vector2d<double >>();
+		std::vector < Vector2d<double >> positions = std::vector<Vector2d<double >>();
+	
+		Universe() = default;
+
+		BoundingBox get_bounding_box(){
+			if (num_bodies == 0) {
+				return BoundingBox(0, 0, 0, 0);
+			}
+		
+			//Initializing at the first body
+			double min_x = positions[0][0];
+			double max_x = positions[0][0];;
+			double min_y = positions[0][1];;
+			double max_y = positions[0][1];;
+			for (Vector2d<double> body : positions) {
+				if (body[0] < min_x) min_x = body[0];
+				if (body[0] > max_x) max_x = body[0];
+				if (body[1] < min_y) min_y = body[1];
+				if (body[1] > max_y) max_y = body[1];
+
+			}
+			return BoundingBox(min_x, max_x, min_x, max_y);
+	
+	}
 
 };
--- source/structures/vector2d.h
+++ source/structures/vector2d.h
@@ -1,40 +1,41 @@
 #pragma once
-template<typename _t1>
-class Vector2d {
-    _t1 x;
-    _t1 y;
-    public: Vector2d(_t1 new_x, _t1 new_y): x{new_x}, y{new_y} {};
-        Vector2d() = default;
+template<typename _Ty>
+    class Vector2d {
+        _Ty x;
+        _Ty y;
+        public: Vector2d(_Ty new_x, _Ty new_y) : x{ new_x }, y{ new_y } {};
+                Vector2d() = default;
 
-        _t1 operator [] (int index) {
-            if (index==0){
-                return this.x;
-            }
-            else if (index==1){
-                return this.y;
-            }
-            else {
-                throw std::exception{};
-            }
-        }
-        void set(_t1 new_x, _t1 new_y){
-            this.x=new_x;
-            this.y=new_y;
-        } 
-        Vector2d operator + (Vector2d other){
-            return new Vector2d(this[0]+other[0], this[1]+other[1]);
-        }
-        Vector2d operator * (Vector2d other){
-            return new Vector2d(this[0]*other[0], this[1]*other[1]);
-        }
-        Vector2d operator - (Vector2d other){
-            return new Vector2d(this[0]-other[0], this[1]-other[1]);
-        }
-        Vector2d operator / (Vector2d other){
-            return new Vector2d(this[0]/other[0], this[1]/other[1]);
-        }
-        bool operator == (Vector2d other){
-            return (this[0]==other[0] && this[1]==other[1]);
-        }
+                _Ty operator [] (int index) {
+                    if (index == 0) {
+                        return x;
+                    }
+                    else if (index == 1) {
+                        return y;
+                    }
+                    else {
+                        throw std::exception{};
+                    }
+                };
+                void set(_Ty new_x, _Ty new_y) {
+                    x = new_x;
+                    y = new_y;
+                };
+                Vector2d operator + (Vector2d<_Ty> other){
+                    return Vector2d(x+other[0], y+other[1]);
+                }
+                Vector2d operator * (_Ty other){
+                    return Vector2d(x*other, y*other);
+                }
+                Vector2d operator - (Vector2d<_Ty> other){
+                    Vector2d result = Vector2d(x + other[0], y + other[1]);
+                    return result;
+                }
+                Vector2d operator / (const _Ty other) {
+                    return Vector2d(x * other, y * other);
+                };
+                bool operator == (Vector2d<_Ty> other) {
+                    return (x == other[0] && y==other[1]);
+                };
 
-    };
+            };
