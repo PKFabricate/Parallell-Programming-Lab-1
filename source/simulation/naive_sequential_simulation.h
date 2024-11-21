ab620147ec5ae63147ae21d46e6c8b8303778d57
PKFabricate <151477741+PKFabricate@users.noreply.github.com>
2024-11-21 14:02:43 +0100

    Whoops. <= changed to < strict

--- source/simulation/naive_sequential_simulation.h
+++ source/simulation/naive_sequential_simulation.h
@@ -7,7 +7,7 @@
 class NaiveSequentialSimulation{
 public:
     static void simulate_epochs(Plotter& plotter, Universe& universe, std::uint32_t num_epochs, bool create_intermediate_plots, std::uint32_t plot_intermediate_epochs) {
-        for (int i = 0; i <= num_epochs; i++) { simulate_epoch(plotter, universe,create_intermediate_plots, plot_intermediate_epochs);}
+        for (int i = 0; i <num_epochs; i++) { simulate_epoch(plotter, universe,create_intermediate_plots, plot_intermediate_epochs);}
     };
 
     static void simulate_epoch(Plotter& plotter, Universe& universe, bool create_intermediate_plots, std::uint32_t plot_intermediate_epochs) {
